59bee5a662b5308e603e0bece64fa636afb4e34c
rishabhthareja <rishabhthareja.2007@gmail.com>
2023-07-19 11:09:06 +0530

    Added comment for each command and added new sentence

--- NLP_tokenize.py
+++ NLP_tokenize.py
@@ -4,11 +4,16 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 
 sentences = [
     'I love my dog',
-    'I love my cat'
+    'I love my cat',
+    'You love my dog!'
     ]
 
-
+# Initialize the Tokenizer class
 tokenizer = Tokenizer(num_words = 100)
+
+# Generate indices for each word in the corpus
 tokenizer.fit_on_texts(sentences)
+
+# Get the indices and print it
 word_index = tokenizer.word_index
 print(word_index)
